0fcc35fce38f1b23cecbf3b13622102028c68dcc
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-03-25 14:35:20 +0200

    complete caesarCipher

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,7 +1,22 @@
-function caesarCipher(val, num) {
-    f
-    return cipher
+function caesarCipher(string, shift) {
+    let newString = "";
+    let Pattern = /[A-Z]|[a-z]/g;
+    
+    for (let i = 0; i < string.length; i++) {
+    if (string[i].match(Pattern)) {
+      let shiftedCode = string.charCodeAt(i) + shift;
+      let shiftedChar = String.fromCharCode(shiftedCode);
+
+      //if the character after shift not in range [a-z] || [A - Z] so wrapping
+      if (!shiftedChar.match(Pattern)) {
+        shiftedChar = String.fromCharCode(shiftedCode - 26);
+      }
+      newString += shiftedChar;
+      continue;
+    }
+    newString += string[i];
+  }
+  return newString;
 }
 
-console.log(caesarCipher("abc"))
-module.export = caesarCipher;
+module.exports = caesarCipher;
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,19 +1,25 @@
 const caesarCipher = require("../src/caesarCipher")
 
 describe("caesarCipher", () => {
-    test("Cipher works", () => {
-        expect(caesarCipher("abc", 1)).toBe("bcd")
-    });
-    test("Cipher wraps from z to a", () => {
-        expect(caesarCipher("xyz", 3)).toBe("abc")
-    });
-    test("Cipher has case preservation", () => {
-        expect(caesarCipher("HeLLo", 3)).toBe("KhOOr")
-    });
-    test("Cipher maintains punctuation and spacing", () => {
-        expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!")
-    });
-    test("Cipher works", () => {
-        expect(caesarCipher("abc", 1)).toBe("bcd")
-    });
-})
+  test("test shifted", () => {
+    expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  });
+  test("test with number", () => {
+    expect(caesarCipher("123", 3)).toBe("123");
+  });
+  test("test with other symbols", () => {
+    expect(caesarCipher("12@#!ab3", 3)).toBe("12@#!de3");
+  });
+  test("test wrapping", () => {
+    expect(caesarCipher("xyz", 4)).toBe("bcd");
+  });
+  test("test itself", () => {
+    expect(caesarCipher("abc", 26)).toBe("abc");
+  });
+  test("test capitalize", () => {
+    expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+  });
+  test("test blank", () => {
+    expect(caesarCipher("", 3)).toBe("");
+  });
+});
